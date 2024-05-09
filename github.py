de5b169ffe1f07b5bbbde29da05b3b2b0da84bdc
jaysonquito <165160915+jaysonquito@users.noreply.github.com>
2024-05-09 20:46:10 +0800

    Update github.py

--- github.py
+++ github.py
@@ -1,2 +1,16 @@
-name = "Jayson"
-print (name)
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREN_HEIGHT))
+
+player = pygame.Rect((300,250,50,50))
+
+run = True
+while run:
+
+    pygame.draw.rect(screen, (255, 0 ,0), player)
+
